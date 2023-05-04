8235aaf071c06008427863015dc9b53eeab95961
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-04 12:25:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,12 @@ import folium
 def draw_chart(root):
     with open(root, 'rb') as file:
         fig = pickle.load(file)
-        if isinstance(fig, plt.Figure):
-            st.pyplot(fig)
-        elif isinstance(fig, folium.Map):
-            folium_static(fig)
-        else:
-            st.warning("Unknown chart type")
+        fig.show()
 
 st.title('서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
 
 draw_chart("barchart.pickle")
 draw_chart("barchart2.pickle")
 draw_chart("barchart3.pickle")
+draw_chart("mapchart.pickle")
 
