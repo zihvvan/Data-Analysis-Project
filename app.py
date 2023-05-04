3d9de64681abd40268cb8e591bee0248c0ba62c9
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-04 11:55:03 +0900

    1

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 import streamlit as st
 import pickle
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from streamlit_folium import folium_static
 import folium
-
 
 def draw_chart(root):
     with open(root, 'rb') as file:
-       fig = pickle.load(file)
-       fig.show()
-        
+        fig = pickle.load(file)
+        if isinstance(fig, plt.Figure):
+            st.pyplot(fig)
+        elif isinstance(fig, folium.Map):
+            folium_static(fig)
+        else:
+            st.warning("Unknown chart type")
 
-    
-st.title(' 서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
+st.title('서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
 
 draw_chart("barchart.pickle")
 draw_chart("barchart2.pickle")
 draw_chart("barchart3.pickle")
 draw_chart("mapchart.pickle")
-
-
-    
-
-    
