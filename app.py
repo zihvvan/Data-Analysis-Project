942616dd6ca56bf753456677cf070d3267d8f135
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 00:59:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 import folium
 
+def draw_image(root):
+image = Image.open(root)
+st.image(image, caption='', use_column_width=True)
 
 def draw_chart(root):
     with open(root, 'rb') as file:
@@ -13,15 +16,19 @@ def draw_chart(root):
         else:
             st.warning("Unknown chart type")
             
-def draw_image(root):
-image = Image.open(root)
-st.image(image, caption='', use_column_width=True)
+
 
 st.set_option('deprecation.showfileUploaderEncoding', False) # streamlit 0.84.0 버전 이후 업로드 파일명 인코딩 에러 대처
 
 st.title('서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
 
-draw_image("charger_graph.png")
-draw_image("charger_per_1000_graph.png")
-draw_image("graph.png")
+charger_graph = plt.imread("charger_graph.png")
+draw_chart(charger_graph)
+
+charger_per_1000_graph = plt.imread("charger_per_1000_graph.png")
+draw_chart(charger_per_1000_graph)
+
+seoul_electric_car_map = plt.imread("seoul_electric_car_map.png")
+st.image(seoul_electric_car_map)
+
 draw_chart("seoul_electric_car_map.joblib")
