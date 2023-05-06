f27bf095720519c1f99a69ead54f430e41b526fa
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 00:47:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import streamlit as st
-import pickle
+import joblib
 import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 import folium
-import joblib
 
 def draw_chart(root):
     with open(root, 'rb') as file:
-        fig = pickle.load(file)
+        fig = joblib.load(file)
         if isinstance(fig, plt.Figure):
             st.pyplot(fig)
         elif isinstance(fig, folium.Map):
@@ -18,9 +17,7 @@ def draw_chart(root):
 
 st.title('서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
 
-draw_chart("charger_graph.pkl")
-draw_chart("charger_per_1000_graph.pkl")
-draw_chart("graph.pickle")
-draw_chart("seoul_electric_car_map.pickle")
-
-
+draw_chart("charger_graph.joblib")
+draw_chart("charger_per_1000_graph.joblib")
+draw_chart("graph.joblib")
+draw_chart("seoul_electric_car_map.joblib")
