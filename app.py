6467e3f028fe6e1ec0b53bd24ffaeae5ff92b44d
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 01:59:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,13 +24,6 @@ def main():
     image = Image.open('graph.png')
     st.image(image, caption='서울특별시 연도별 전기차 등록대수(사업용/비사업)', use_column_width=True)
     st.markdown("---")
-def main():
-    # Load the map from a pickled file
-    map_html = joblib.load("map.pkl")
-
-    # Display the map
-    st.markdown("<h1 style='text-align: center;'>Seoul EV Charger Locations</h1>", unsafe_allow_html=True)
-    st.markdown(map_html, unsafe_allow_html=True)
 
 if __name__ == '__main__':
     main()
