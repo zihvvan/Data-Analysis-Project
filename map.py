00e4f5128b2fe0745f98cefb6d8ffa7da6e1d985
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 02:28:12 +0900

    Update map.py

--- map.py
+++ map.py
@@ -2,9 +2,12 @@ import folium
 import joblib
 import streamlit as st
 
+def main():
+    # Load map in Streamlit
+    st.set_page_config(page_title="Map Viewer")
+    st.markdown("<h1 style='text-align: center;'>Seoul EV Charger Locations</h1>", unsafe_allow_html=True)
+    map_html = joblib.load("map.pkl")
+    st.markdown(map_html, unsafe_allow_html=True)
 
-# Load map in Streamlit
-st.set_page_config(page_title="Map Viewer")
-st.markdown("<h1 style='text-align: center;'>Seoul EV Charger Locations</h1>", unsafe_allow_html=True)
-map_html = joblib.load("map.pkl")
-st.markdown(map_html, unsafe_allow_html=True)
+if __name__ == "__main__":
+    main()
