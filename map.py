10fdb176abfb14af3187a45e992a26ca18e19ed2
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 02:29:55 +0900

    Update map.py

--- map.py
+++ map.py
@@ -3,6 +3,8 @@ import joblib
 import streamlit as st
 
 def main():
+    # 서울특별시 전기차 충전기 위치 및 개수
+    st.header('서울특별시 전기차 충전기 위치 및 개수')
     # Load map in Streamlit
     st.set_page_config(page_title="Map Viewer")
     st.markdown("<h1 style='text-align: center;'>Seoul EV Charger Locations</h1>", unsafe_allow_html=True)
