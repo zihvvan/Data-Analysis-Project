788ee74dcbd65450f5f6ae5ed6957a18c8589207
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 02:18:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from streamlit.server.Server import Server
+from streamlit.state.session_state import SessionState
 from chart import main as chart_page
 from map import main as map_page
 
 def main():
-    server = Server.get_current()
-    session = server.session_state.get(page="chart", data=42) # create a session state
+    session = SessionState.get(page="chart", data=42) # create a session state
 
     if session.page == "chart":
         chart_page()
