13d61a9d5a6e22d5c2d419de3dc8aed653dd0391
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 01:23:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import folium
 import joblib
 
 def main():
-    st.title('서울특별시 데이터 시각화 \n(전기차/충전소) 프로젝트')
+    st.header('서울특별시 데이터 시각화')
+    st.subheader('(전기차/충전소) 프로젝트')
     
     # 지자체별 전기차 충전기(급속/완속) 갯수
     st.header('지자체별 전기차 충전기(급속/완속) 갯수')
