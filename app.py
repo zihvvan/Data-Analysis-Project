5b76ba3e0c7b8c24b00d31022a8c68ef05cf15ba
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 00:58:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,24 @@ import matplotlib.pyplot as plt
 from streamlit_folium import folium_static
 import folium
 
-# 나눔 폰트 설치
-!sudo apt-get install -y fonts-nanum
-!sudo fc-cache -fv
-!rm ~/.cache/matplotlib -rf
-
-# matplotlib에서 한글폰트 사용을 위한 설정
-plt.rc('font', family='NanumBarunGothic') 
-
-# streamlit에서 한글폰트 사용을 위한 설정
-st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def draw_chart(root):
     with open(root, 'rb') as file:
         fig = joblib.load(file)
-        if isinstance(fig, plt.Figure):
-            st.pyplot(fig)
-        elif isinstance(fig, folium.Map):
+        if isinstance(fig, folium.Map):
             folium_static(fig)
         else:
             st.warning("Unknown chart type")
             
-st.set_option('deprecation.showfileUploaderEncoding', False) # streamlit 0.84.0 버전 이후 업로드 파일명 인코딩 에러 대처
+def draw_image(root):
+image = Image.open(root)
+st.image(image, caption='', use_column_width=True)
 
+st.set_option('deprecation.showfileUploaderEncoding', False) # streamlit 0.84.0 버전 이후 업로드 파일명 인코딩 에러 대처
 
 st.title('서울특별시 데이터 시각화 (전기차/충전소) 프로젝트')
 
-draw_chart("charger_graph.joblib")
-draw_chart("charger_per_1000_graph.joblib")
-draw_chart("graph.joblib")
+draw_image("charger_graph.png")
+draw_image("charger_per_1000_graph.png")
+draw_image("graph.png")
 draw_chart("seoul_electric_car_map.joblib")
