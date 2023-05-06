f060be1d4f7720b710b9a264851e891c0ad997b7
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-07 02:13:29 +0900

    Update map.py

--- map.py
+++ map.py
@@ -2,15 +2,6 @@ import folium
 import joblib
 import streamlit as st
 
-# Create map
-map = folium.Map(location=[37.566345, 126.977893], zoom_start=12, width='%100', height='%100')
-locations = list(zip(df3.lat, df3.lon))
-from folium import plugins
-cluster = plugins.MarkerCluster(locations=locations, popups=df3["addr"].tolist())
-map.add_child(cluster)
-
-# Save map using joblib
-joblib.dump(map._repr_html_(), "map.pkl")
 
 # Load map in Streamlit
 st.set_page_config(page_title="Map Viewer")
