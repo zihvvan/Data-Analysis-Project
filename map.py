4d7d7d3e81f1fa6808f40eb335f24132bb64a5c8
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-08 11:29:13 +0900

    Update map.py

--- map.py
+++ map.py
@@ -1,12 +1,18 @@
 import folium
 import joblib
 import streamlit as st
+import pandas as pd
 
+# CSV 파일을 Pandas DataFrame으로 읽어들임
+df = pd.read_csv("서울특별시 전기차 충전소.csv")
 
-def main():
-    # Load map in Streamlit
-    map_html = joblib.load("map.pkl")
-    st.markdown(map_html, unsafe_allow_html=True)
+# 데이터프레임 출력
+st.dataframe(df3)
+
+# def main():
+#     # Load map in Streamlit
+#     map_html = joblib.load("map.pkl")
+#     st.markdown(map_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
