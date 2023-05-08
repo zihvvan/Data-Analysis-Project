479acaf6f36011e3d9ad1de4e33cbf825af82e25
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-05-08 12:04:23 +0900

    Update map.py

--- map.py
+++ map.py
@@ -30,29 +30,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-
-# def main():    
-#     st.title('데이터 시각화 프로젝트')
-#     st.subheader('서울특별시 전기차 충전소 위치🏁')
-#     st.markdown("---")
-#     # CSV 파일을 Pandas DataFrame으로 읽어들임
-#     df3 = pd.read_csv("서울특별시 전기차 충전소.csv")
-
-#     # 데이터프레임 출력
-#     st.dataframe(df3)
-#     st.markdown("---")
-#     # Create a map centered on Seoul
-#     m = folium.Map(location=[37.566345, 126.977893])
-
-#     # Add markers for each charging station
-#     for i in range(len(df3)):
-#         lat = df3.loc[i, "lat"]
-#         lon = df3.loc[i, "lon"]
-#         folium.Marker([lat, lon]).add_to(m)
-
-#     # Render map using Folium
-#     folium_static(m)
-
-# if __name__ == "__main__":
-#     main()
-
