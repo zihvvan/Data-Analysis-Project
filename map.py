4734ae97dd101727472c6b2cbd0a473b83d66387
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-06-18 00:48:39 +0900

    Update map.py

--- map.py
+++ map.py
@@ -45,14 +45,12 @@ import streamlit as st
 import pandas as pd
 import folium
 from folium.plugins import FastMarkerCluster
-import branca.element as be
 
 
 def main():
     st.title('데이터 시각화 프로젝트')
     st.subheader('서울특별시 전기차 충전소 위치🏁')
     st.markdown("---")
-    
     # CSV 파일을 Pandas DataFrame으로 읽어들임
     df3 = pd.read_csv("서울특별시 전기차 충전소.csv")
 
@@ -60,9 +58,7 @@ def main():
     st.header("서울특별시 전기차 충전소.csv 📄")
     st.dataframe(df3)
     st.markdown("---")
-    
     st.header("서울특별시 전기차 충전소 지도 🗺")
-    
     # Create a map centered on Seoul
     m = folium.Map(location=[37.566345, 126.977893], zoom_start=11)
 
@@ -72,25 +68,27 @@ def main():
 
     # Add popup to each marker
     for idx, row in df3.iterrows():
-        popup = f"{row['stat_nm']}<br>{[row['addr']]}<br>{[row['charger_type']]}"
+        popup = f"{row['stat_nm']}<br>{row['addr']}<br>{row['charger_type']}"
         tooltip = row['stat_nm']
-        
         folium.Marker(
             location=[row['lat'], row['lon']],
             popup=popup,
             tooltip=tooltip,
         ).add_to(marker_cluster)
 
-    # Render map as HTML into an IFrame
-    m_html = m._repr_html_()
-    iframe_component = be.IFrame(html=m_html, width="100%", height="600px")
+    # Save the map as an HTML file
+    m.save('map.html')
     
-    # Display the IFrame as a Streamlit component
-#     st.components.v1.html(iframe_component.to_html(), height=600)
-    st.markdown(iframe_component.to_html(), unsafe_allow_html=True)
+    # Display the map using an iframe
+    st.markdown('<iframe src="map.html" width="1000" height="500"></iframe>', unsafe_allow_html=True)
+    st.markdown("---")
+
 
 if __name__ == "__main__":
     main()
+
+
+
 
 
 
