056a9bbf47ed102773bf1c23d8990f0a9830d75b
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-06-20 20:43:29 +0900

    Update map.py

--- map.py
+++ map.py
@@ -29,14 +29,13 @@ def main():
 #     return f'<a href="{link_url}" target="_blank"><img src="data:image/png;base64,{encoded_image}"></a>'
 
 
-      button_clicked = st.button("Click to Open Link")
-      if button_clicked:
-          js_code = f"window.open('{link_url}')"  # 새 창에서 링크 열기
-          st.markdown(f'<p style="text-align:center"><a href="#" onclick="{js_code}">Click to Open Link</a></p>', unsafe_allow_html=True)
-
     #  # 전기차 충전소 지도 링크 연결
     # if st.button("Click to Open Link"):
     #     st.markdown(f"[링크 바로가기]({link_url})")
+
+    # 전기차 충전소 지도 링크 연결
+   
+    st.button(f"[링크 바로가기]({link_url})")
 
 
     # 이미지를 streamlit에 추가
